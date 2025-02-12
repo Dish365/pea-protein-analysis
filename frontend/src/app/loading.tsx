1c0d938ed132244e34722f221ff9a34d38c26e0d
eakwofie <amankrahkwofie354@gmail.com>
2025-02-12 08:03:05 -0500

    updated the loaded

--- frontend/src/app/loading.tsx
+++ frontend/src/app/loading.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LoadingSpinner from "@/components/common/LoadingSpinner";
+import { LoadingSpinner } from "@/components/ui/loading-spinner";
 
 export default function Loading(): React.JSX.Element {
   return <LoadingSpinner tip="Loading application..." />;
